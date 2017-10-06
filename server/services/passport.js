f4cdbe5a87bfcbc2a0a94350a163b015ae61779e
Waqas84 <walsubayee@yahoo.com>
2017-10-06 14:02:36 -0700

    modified promises using Async & Await

--- server/services/passport.js
+++ server/services/passport.js
@@ -9,8 +9,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-	User.findById(id)
-	.then(user => {
+	User.findById(id).then(user => {
 		done(null, user);
 	});
 });
@@ -26,25 +25,22 @@ passport.use(
 			callbackURL: "/auth/google/callback",
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id })
-			    .then(existingUser => {
-				if (existingUser) {
-					console.log("--------------------------------");
-					console.log("User is Exist");
-					console.log("User Google ID : ", profile.id);
-					console.log("--------------------------------");
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id })
-						.save()
-						.then(user => done(null, user));
-					console.log("--------------------------------");
-					console.log("User Google ID : ", profile.id);
-					console.log("User is Registered");
-					console.log("--------------------------------");
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				console.log("--------------------------------");
+				console.log("user is already exist");
+				console.log("user Google ID : ", profile.id);
+				console.log("--------------------------------");
+				return done(null, existingUser);
+			}
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
+			console.log("--------------------------------");
+			console.log("user Google ID : ", profile.id);
+			console.log("user has been registered");
+			console.log("--------------------------------");
 		}
 	)
 );
