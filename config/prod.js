c265a9e5e79bf3e06c48deebe45ec44e2a11dffe
Waqas84 <walsubayee@yahoo.com>
2017-10-05 17:09:58 -0700

    Production env working

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	MONGOURI: process.env.MONGODB_URI,
+	cookieKey: process.env.COOKIE_KEY
+};
+
+
