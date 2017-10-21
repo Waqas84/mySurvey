3cf54549dee5fee4ba28072fddf62bdf2e32da6b
Waqas84 <walsubayee@yahoo.com>
2017-10-21 15:39:21 -0700

    added the auth action creator

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { FETCH_USER } from "./types";
+
+// export const fetchUser = () => async dispatch =>
+// 	dispatch({
+// 		type: FETCH_USER,
+// 		payload: await axios.get("/api/current_user")
+// 	});
+
+export const fetchUser = () => {
+	return function(dispatch) {
+		axios
+			.get("/api/current_user")
+			.then(res => dispatch({ type: FETCH_USER, payload: res }));
+	};
+};
